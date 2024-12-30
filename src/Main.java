80625feed8e3b2b9ca4502c64902983179d4c23b
Kanika <kanika.skyquestt@gmail.com>
2024-12-30 16:33:48 +0530

    Custom linked list

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CustomLinkedList cll = new CustomLinkedList();
+        cll.insertFirst(1);
+        cll.insertFirst(19);
+        cll.insertFirst(2);
+        cll.insertFirst(5);
+        cll.insertFirst(8);
+        cll.insertLast(99);
+        cll.insert(100, 3);
+        cll.display();
+        System.out.println(cll.deleteFirst());
+        cll.display();
+    }
+}
